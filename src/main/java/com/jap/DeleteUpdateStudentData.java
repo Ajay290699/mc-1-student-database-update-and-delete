7c9ec0b2a6f2e67de5b54355b7550c6216d0917f
Ajay Chavan <ajayc9838@gmail.com>
2022-09-13 15:30:33 +0530

    completed

--- src/main/java/com/jap/DeleteUpdateStudentData.java
+++ src/main/java/com/jap/DeleteUpdateStudentData.java
@@ -1,36 +1,57 @@
 package com.jap;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Hello world!
- *
  */
-public class DeleteUpdateStudentData
-{
+public class DeleteUpdateStudentData {
 
-    public Connection getConnection(){
-        Connection connection=null;
+    public static void main(String[] args) {
+        DeleteUpdateStudentData salesDataDemo = new DeleteUpdateStudentData();
+        try {
+            salesDataDemo.deleteStudentData();
+            salesDataDemo.updateStudentData();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Connection connection = null;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/school";
+        String user = "root";
+        String pass = "Savita@123";
+        connection = DriverManager.getConnection(url, user, pass);
 
         return connection;
 
     }
 
-    public void deleteStudentData(){
-      // call getConnection() method
-       
-        //execute delete query
-    }
-
-    public void updateStudentData(){
+    public void deleteStudentData() throws SQLException, ClassNotFoundException {
         // call getConnection() method
-
-        //execute update query
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+        //execute delete query
+        String sql = "delete from student1 where rollNo = 4";
+        int rows = statement.executeUpdate(sql);
+        System.out.println("rows = " + rows);
     }
 
-    public static void main(String[] args) {
-        DeleteUpdateStudentData salesDataDemo = new DeleteUpdateStudentData();
-        salesDataDemo.deleteStudentData();
-        salesDataDemo.updateStudentData();
+    public void updateStudentData() throws SQLException, ClassNotFoundException {
+        // call getConnection() method
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+        //execute update query
+        String sql = "update student1 set Name = 'Bala' where rollNo = 5 and marks = 90";
+        int rows = statement.executeUpdate(sql);
+        System.out.println("rows = " + rows);
     }
 }
